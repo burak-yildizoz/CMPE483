95d65119daa8e112641079383187962f125b1d7f
burak-yildizoz <45322678+burak-yildizoz@users.noreply.github.com>
2021-01-03 17:57:37 +0300

    BUlOT.sol is now compilable
    most functions are not implemented yet

--- BULOT.sol
+++ BULOT.sol
@@ -1,47 +1,107 @@
-//pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >=0.4.22 <0.7.0;
+// SPDX-License-Identifier: AGPL-3.0-only
+
 //import "./EIP20.sol";
 
 contract BULOT{
     //.data
     //EIP20 TL_BANK;
-    //mapping(uint => mapping(uint => bytes32)) hashes;     // database for hash of random numbers stored:   first index: lottery_no, second index: ticket_no
-    //mapping(uint => bytes32) lotteryRandom                // list for the random numbers calculated for each week's lottery. // UPDATE TAVSİYESİ: her reveal ardından lotteryRandom = sha3(lotteryRandom, revealedrandom)
-    //mapping(uint => mapping(uint=> address)) ticketowner  // (lotteryno, ticketno) => owner   // to authenticate withdraw // we could make ticket_no unique to avoid some extra storage
-    //mapping(uint => mapping(uint => bool)) notrevealed    // (lotteryno, ticketno) => redeemable (reveal etmeyen adam sonradan ödül de alamaz)
+    mapping(uint => mapping(uint => bytes32)) hashes;     // database for hash of random numbers stored:   first index: lottery_no, second index: ticket_no
+    mapping(uint => bytes32) lotteryRandom;               // list for the random numbers calculated for each week's lottery. // UPDATE TAVSİYESİ: her reveal ardından lotteryRandom = sha3(lotteryRandom, revealedrandom)
+    mapping(uint => mapping(uint=> address)) ticketowner; // (lotteryno, ticketno) => owner   // to authenticate withdraw // we could make ticket_no unique to avoid some extra storage
+    mapping(uint => mapping(uint => bool)) notrevealed;   // (lotteryno, ticketno) => redeemable (reveal etmeyen adam sonradan ödül de alamaz)
 
     //.code
     //implement constructor
     //implement fallback (in case someone sends ethers to the contract)
     // ? it may make sense to implement reward calculator as a pure function // RANDOM TAVSİYESİ: i'nci ödülü kazanan ticket ID = sha3(lotteryRandom,i) % M
 
-    function buyTicket              (bytes32 hash_rnd_number)           public //returns (uint)
+    function buyTicket              (bytes32 hash_rnd_number)           public returns (uint) {
+        require(false, "Not implemented yet");
         //require(TL_BANK.transferFrom(msg.sender, address(this), 1)); //buna bir de exception handling lazım olabilir?
+    }
 
 
-    function revealRndNumber        (uint ticketno, uint rnd_number)    public // zamanı kontrol etmeye gerek yok, zaten hash tutmaz o zaman.
+    function revealRndNumber        (uint ticketno, uint rnd_number)    public { // zamanı kontrol etmeye gerek yok, zaten hash tutmaz o zaman.
+        require(false, "Not implemented yet");
         //require(sha3(rnd_number) == hashes[this.currentWeek()-1][ticketno]);
+    }
 
 
-    function withdrawTicketPrize    (uint lottery_no, uint ticket_no)   public   // eğer unique ticket_no kullanırsak ilk argümana gerek kalmayabilir.
+    function withdrawTicketPrize    (uint lottery_no, uint ticket_no)   public { // eğer unique ticket_no kullanırsak ilk argümana gerek kalmayabilir.
+        require(false, "Not implemented yet");
         //require(ticketowner[lottery_no][ticket_no])
         //require(notrevealed[lottery_no][ticket_no])      // bu satır erc20'deki allowed olayına bakarak değiştirilebilir, ayrıca üstteki require içinde &&'lanabilir.
         //uint amount = this.checkIfTicketWon(lottery_no, ticket_no)
         // SETTLED: This will call checkIfTicketWon()
         // burada notrevealed'dan lottery_no, ticket_no kısmı silinebilir.
+    }
 
-    /*function currentWeek          ()                                  private returns (uint) {
-        return block.timestamp / SECONDS_PER_WEEK; //now \equiv block.timestamp
-    }*/
+    function currentWeek            ()                                  private view returns (uint) {
+        return block.timestamp / /*SECONDS_PER_WEEK*/ (60*60*24*7);
+    }
 
+    // https://ethereum.stackexchange.com/questions/8086/logarithm-math-operation-in-solidity#30168
+    function log_2                  (uint x)                            private view returns (uint y) {
+        assembly {
+            let arg := x
+            x := sub(x,1)
+            x := or(x, div(x, 0x02))
+            x := or(x, div(x, 0x04))
+            x := or(x, div(x, 0x10))
+            x := or(x, div(x, 0x100))
+            x := or(x, div(x, 0x10000))
+            x := or(x, div(x, 0x100000000))
+            x := or(x, div(x, 0x10000000000000000))
+            x := or(x, div(x, 0x100000000000000000000000000000000))
+            x := add(x, 1)
+            let m := mload(0x40)
+            mstore(m,           0xf8f9cbfae6cc78fbefe7cdc3a1793dfcf4f0e8bbd8cec470b6a28a7a5a3e1efd)
+            mstore(add(m,0x20), 0xf5ecf1b3e9debc68e1d9cfabc5997135bfb7a7a3938b7b606b5b4b3f2f1f0ffe)
+            mstore(add(m,0x40), 0xf6e4ed9ff2d6b458eadcdf97bd91692de2d4da8fd2d0ac50c6ae9a8272523616)
+            mstore(add(m,0x60), 0xc8c0b887b0a8a4489c948c7f847c6125746c645c544c444038302820181008ff)
+            mstore(add(m,0x80), 0xf7cae577eec2a03cf3bad76fb589591debb2dd67e0aa9834bea6925f6a4a2e0e)
+            mstore(add(m,0xa0), 0xe39ed557db96902cd38ed14fad815115c786af479b7e83247363534337271707)
+            mstore(add(m,0xc0), 0xc976c13bb96e881cb166a933a55e490d9d56952b8d4e801485467d2362422606)
+            mstore(add(m,0xe0), 0x753a6d1b65325d0c552a4d1345224105391a310b29122104190a110309020100)
+            mstore(0x40, add(m, 0x100))
+            let magic := 0x818283848586878898a8b8c8d8e8f929395969799a9b9d9e9faaeb6bedeeff
+            let shift := 0x100000000000000000000000000000000000000000000000000000000000000
+            let a := div(mul(x, magic), shift)
+            y := div(mload(add(m,sub(255,a))), shift)
+            y := add(y, mul(256, gt(arg, 0x8000000000000000000000000000000000000000000000000000000000000000)))
+        }
+    }
 
 
     //.view
-    function getLastBoughtTicketNo  (uint lottery_no)                   public view returns (uint)
-    function getIthBoughtTicketNo   (uint i, uint lottery_no)           public view returns (uint)
-    function checkIfTicketWon       (uint lottery_no, uint ticket_no)   public view returns (uint amount) // SETTLED: This will call getIthWinningTicket for all I
-    function getIthWinningTicket    (uint i, uint lottery_no)           public view returns (uint ticket_no, uint amount)
-    function getCurrentLotteryNo    ()                                  public view returns (uint lottery_no)
+    function getLastBoughtTicketNo  (uint lottery_no)                   public view returns (uint) {
+        require(false, "Not implemented yet");
+    }
+    function getIthBoughtTicketNo   (uint i, uint lottery_no)           public view returns (uint) {
+        require(ticketowner[lottery_no][i] != address(0), "Ticket is not sold");
+        return i;
+    }
+    function checkIfTicketWon       (uint lottery_no, uint ticket_no)   public view returns (uint amount) { // SETTLED: This will call getIthWinningTicket for all I
+        require(false, "Not implemented yet");
+        for (uint i=0; i<log_2(getLastBoughtTicketNo(lottery_no)); i++)
+        {
+            (uint ith_ticket_no, uint ith_amount) = getIthWinningTicket(i, lottery_no);
+            if (ith_ticket_no == ticket_no)
+                return ith_amount;
+        }
+        return 0;
+    }
+    function getIthWinningTicket    (uint i, uint lottery_no)           public view returns (uint ticket_no, uint amount) {
+        require(false, "Not implemented yet");
+    }
+    function getCurrentLotteryNo    ()                                  public view returns (uint lottery_no) {
+        return currentWeek();
+    }
     function getMoneyCollected      (uint lottery_no)                   public view returns (uint amount)
+    {
+        require(false, "Not implemented yet");
+    }
 
 }
 
