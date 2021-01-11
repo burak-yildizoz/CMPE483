1572b00b5a6803bb596b99b3c0e9bc002af01b6a
Muhammed Enes Toptaş <44070728+EnesToptas@users.noreply.github.com>
2021-01-12 02:03:34 +0300

    Initial test file, more to be added

---
+++ test.js
@@ -0,0 +1,511 @@
+erc20abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_initialAmount",
+                "type": "uint256"
+            },
+            {
+                "internalType": "string",
+                "name": "_tokenName",
+                "type": "string"
+            },
+            {
+                "internalType": "uint8",
+                "name": "_decimalUnits",
+                "type": "uint8"
+            },
+            {
+                "internalType": "string",
+                "name": "_tokenSymbol",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "remaining",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "allowed",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "balance",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "balances",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "internalType": "uint8",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+bulotabi = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "TL_contract",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "stateMutability": "nonpayable",
+        "type": "fallback"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "hash_rnd_number",
+                "type": "bytes32"
+            }
+        ],
+        "name": "buyTicket",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "ticket_no",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "lottery_no",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "ticket_no",
+                "type": "uint256"
+            }
+        ],
+        "name": "checkIfTicketWon",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getCurrentLotteryNo",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "lottery_no",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "i",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "lottery_no",
+                "type": "uint256"
+            }
+        ],
+        "name": "getIthBoughtTicketNo",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "i",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "lottery_no",
+                "type": "uint256"
+            }
+        ],
+        "name": "getIthWinningTicket",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "ticket_no",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "lottery_no",
+                "type": "uint256"
+            }
+        ],
+        "name": "getLastBoughtTicketNo",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "lottery_no",
+                "type": "uint256"
+            }
+        ],
+        "name": "getMoneyCollected",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "ticket_no",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "rnd_number",
+                "type": "uint256"
+            }
+        ],
+        "name": "revealRndNumber",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "lottery_no",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "ticket_no",
+                "type": "uint256"
+            }
+        ],
+        "name": "withdrawTicketPrize",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+erc20address = "0x5a4eBb10942cBf41154F13Ed16cFf57CDE116350"
+bulotaddress = "0x0d3C830D9a79aC2C48290fe73af7039818FC6fe9"
+
+erc20contract = undefined
+bulotcontract = undefined
+
+function getContracts(){
+    erc20contract = web3.eth.contract(erc20abi).at(erc20address);
+    bulotcontract = web3.eth.contract(bulotabi).at(bulotaddress);
+}
+
+function createAccounts(howmany) {
+    for (i=0; i<howmany; i++) {
+        a = personal.newAccount('')
+        web3.eth.sendTransaction({from:eth.accounts[0],to:eth.accounts[i+1],value:1000000000})
+    }
+}
+
+function giveAllowance(howmany) {
+    for (i=0; i< howmany; i++) {
+        eth.defaultAccount=eth.accounts[i];
+        erc20contract.approve(bulotaddress,10);
+    }
+}
+
+function buyTickets(howmany) {
+    for (i=0; i< howmany; i++) {
+        eth.defaultAccount=eth.accounts[i];
+        personal.unlockAccount(eth.accounts[i],'',100);
+        bulotcontract.buyTicket(hash(i*3));
+    }
+}
+
+function hash(input) {
+    return web3.utils.soliditySHA3(input)
+}
+
+getContracts();
+createAccounts(5);
+giveAllowance(5);
+buyTickets(5);
