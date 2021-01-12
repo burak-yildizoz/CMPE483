de7e5727e62c4734aaac0e56bff3e18d1e862a15
Muhammed Enes Toptaş <44070728+EnesToptas@users.noreply.github.com>
2021-01-13 00:00:21 +0300

    several changes, read description
    .call() was not giving the intended results, fixed.
    accounts needed to be unlocked to make transactions, fixed.
    properly works till reveal stage.

--- bulot.js
+++ bulot.js
@@ -1,12 +1,9 @@
 loadScript("erc20tokenabi.js")
 loadScript("bulottokenabi.js")
 
-erc20address = "0x5a4eBb10942cBf41154F13Ed16cFf57CDE116350"
 // change this to the address you deployed with web3 provider in remix
-bulotaddress = "0x0d3C830D9a79aC2C48290fe73af7039818FC6fe9"
-
-var erc20contract = undefined
-var bulotcontract = undefined
+erc20address = "0xb7DbDD3dE04Da2221D6E240546d4f7C9B45c3f32"
+bulotaddress = "0xfc115495Dc73d91EB465547e06B1AF8fbcFB2534"
 
 erc20contract = web3.eth.contract(erc20tokenabi).at(erc20address);
 bulotcontract = web3.eth.contract(bulottokenabi).at(bulotaddress);
@@ -33,35 +30,44 @@ function createAccounts(howmany) {
     }
 }
 
+function givePeopleMoneyToBuyTickets(howmany) {
+    eth.defaultAccount = eth.accounts[0];
+    for (i=1; i< howmany; i++) {
+        erc20contract.transfer(eth.accounts[i], 10);
+    }
+}
+
 function giveAllowance(howmany) {
     for (i=0; i< howmany; i++) {
         eth.defaultAccount=eth.accounts[i];
-        erc20contract.approve(bulotaddress,10);
+        web3.personal.unlockAccount(eth.accounts[i],'',10)
+        erc20contract.approve(bulotaddress, 10);
     }
 }
 
 function buyTickets(howmany) {
     for (i=0; i< howmany; i++) {
         eth.defaultAccount=eth.accounts[i];
+        web3.personal.unlockAccount(eth.accounts[i],'',10);
         thisTicketNo = bulotcontract.buyTicket(hash(i*3));
         ticketsOfPeople[eth.accounts[i]] = thisTicketNo;
     }
 }
 
 function hash(input) {
-    return web3.utils.soliditySHA3(input)
+    return web3.sha3(input);
 }
-/*
-async function sleep(ms) {
-    x = await new Promise(resolve => setTimeout(resolve, ms));
-    return x;
+
+function sleep( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){} 
 }
-*/
+
 function reveal(howmany) {
     for (i=0; i< howmany; i++) {
         eth.defaultAccount=eth.accounts[i];
-        personal.unlockAccount(eth.accounts[i],'',100);
-        bulotcontract.revealRndNumber(ticketsOfPeople[eth.accounts[i]], i*3);
+        personal.unlockAccount(eth.accounts[i],'',10);
+        bulotcontract.revealRndNumber.call(ticketsOfPeople[eth.accounts[i]], i*3);
     }
 }
 
@@ -70,15 +76,15 @@ function getCurrentLotteryNo() {
 }
 
 function getLastBoughtTicketNo(lotteryNo) {
-    return bulotcontract.getLastBoughtTicketNo(lotteryNo);
+    return bulotcontract.getLastBoughtTicketNo.call(lotteryNo);
 }
 
 function checkIfTicketWon(lotteryNo, ticketNo) {
-    return bulotcontract.checkIfTicketWon(lotteryNo, ticketNo);
+    return bulotcontract.checkIfTicketWon.call(lotteryNo, ticketNo);
 }
 
 function getMoneyCollected(lotteryNo) {
-    return bulotcontract.getMoneyCollected(lotteryNo);
+    return bulotcontract.getMoneyCollected.call(lotteryNo);
 }
 
 function getWinningTickets(moneyCollected, lotteryNo) {
@@ -86,26 +92,30 @@ function getWinningTickets(moneyCollected, lotteryNo) {
     winners = [];
 
     for (i=1; i <= howManyDidWin; i++) {
-        var result = bulotcontract.getIthWinningTicket.call(i, lotteryNo)
-        [ticket_no, amount] = result
+        var result = bulotcontract.getIthWinningTicket.call(i, lotteryNo);
+        ticket_no = result[ticket_no];
+        amount = result[amount];
         winners.push([ticket_no, amount]);
     }
 
     return winners;
 }
 
+/*
 createAccounts(5);
 console.log("Lottery no: " + getCurrentLotteryNo())
-/*
+givePeopleMoneyToBuyTickets(5);
 giveAllowance(5);
 buyTickets(5);
-lotteryNo = getCurrentLotteryNo();
 lastTicketNo = getLastBoughtTicketNo(lotteryNo);
-checkIfTicketWon(lotteryNo, lastTicketNo);
+lotteryNo = getCurrentLotteryNo();
+*/
+checkIfTicketWon(lotteryNo, 4);
+/*
 moneyCollected = getMoneyCollected(lotteryNo);
-sleep(1200000);
+sleep(600000);
 reveal(5);
-sleep(1200000);
+sleep(600000);
 nextLotteryNo = getCurrentLotteryNo();
 if(nextLotteryNo == lotteryNo + 1) console.log("next week"); else console.log("not working");
 winners = getWinningTickets(moneyCollected, lotteryNo);
