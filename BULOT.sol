896dd83d6394377da64bf7ed06bf1be60a5d02fa
Selman Berk Özkurt <44111838+SelmanB@users.noreply.github.com>
2021-01-25 20:26:54 +0300

    Update BULOT.sol
    * The hashes storage is now cleaned after use
    * Ticket owner address is now cleaned after prize withdraw (not when someone did not reveal)
    * notcollected data structure cancelled  and existence of ticket owner data structure is used instead
    * notrevealed information is now cleared after use

--- BULOT.sol
+++ BULOT.sol
@@ -11,13 +11,12 @@ contract BULOT
 
     IERC20 private TL_BANK;
     uint constant PRICE = 10;
-    mapping(uint => mapping(uint => bytes32)) hashes;     // database for hash of random numbers stored:   first index: lottery_no, second index: ticket_no
+    mapping(uint => mapping(uint => bytes32)) hashes;     // database for hash of random numbers stored:   first index: lottery_no, second index: ticket_no, --employs cleaning
     mapping(uint => bytes32) lotteryRandom;               // list for the random numbers calculated for each week's lottery
-    mapping(uint => mapping(uint=> address)) ticketowner; // (lotteryno, ticketno) => owner   // to authenticate withdraw // we could make ticket_no unique to avoid some extra storage
+    mapping(uint => mapping(uint=> address)) ticketowner; // (lotteryno, ticketno) => owner   // to authenticate withdraw, --employs cleaning
     mapping(address => uint) lastBoughtTicket;            // owner => owner's last bought ticket no.  Set by buyTicket, read by getLastBoughtTicketNo
-    mapping(uint => mapping(uint => bool)) notrevealed;   // (lotteryno, ticketno) => redeemable (those who do not reveal will not get a prize)
-    mapping(uint => mapping(uint => bool)) notclaimed;    // (lotteryno, ticketno) => remunerable
-    mapping(uint =>uint)  ticketcount;                              // (lotteryno)=> ticketcount  (number of tickets sold for lotteryno). Updated by buyTicket
+    mapping(uint => mapping(uint => bool)) notrevealed;   // (lotteryno, ticketno) => redeemable (those who do not reveal will not get a prize), --employs cleaning
+    mapping(uint =>uint)  ticketcount;                    // (lotteryno)=> ticketcount  (number of tickets sold for lotteryno). Updated by buyTicket
     mapping(uint =>uint) moneycollected;
     mapping(uint =>uint) moneyreturned;
     uint totalmoneycollected;                //failsafe accounting variables
@@ -53,7 +52,6 @@ contract BULOT
         hashes[lottery_no][ticket_no] = hash_rnd_number;                 //Record the hash supplied to authenticate future random number revelation
         ticketowner[lottery_no][ticket_no] = msg.sender;                 //Record the owner of this ticket to check before withdrawal
         notrevealed[lottery_no][ticket_no] = true;                       //Set the variables for checking eligibility for withdrawal in the future
-        notclaimed[lottery_no][ticket_no] = true;
     }
 
     function revealRndNumber        (uint ticket_no, uint rnd_number)    public
@@ -63,20 +61,20 @@ contract BULOT
         require(notrevealed[last_lottery_no][ticket_no], "You have already revealed the ticket");                               //Avoid unnecessary redundant storage updates
         require(keccak256(abi.encode(rnd_number)) == hashes[last_lottery_no][ticket_no], "Your random number is not correct!"); //Authenticate the random number revealed using its previously supplied hash
         //We do not update anything before all checks are made
-        notrevealed[last_lottery_no][ticket_no] = false;                                                                        //Update the state of having revealed
+        delete hashes[last_lottery_no][ticket_no];                                                                              //The hash will not be necessary anymore
+        delete notrevealed[last_lottery_no][ticket_no];                                                                         //Update the state of having revealed
         lotteryRandom[last_lottery_no] = keccak256(abi.encode(lotteryRandom[last_lottery_no], rnd_number));                     //Update the lottery random number according to the technique described in documentation
     }
 
     function withdrawTicketPrize    (uint lottery_no, uint ticket_no)   public
     {
         require(lottery_no < getCurrentLotteryNo() - 1, "Rewards can be claimed after reveal stage ends");                       //Check whether the final lottery random number is found
-        require(ticketowner[lottery_no][ticket_no] == msg.sender, "Only the ticket owner can claim reward");                     //Authenticate that entity willing to receive the price is who bought this ticket
         require(notrevealed[lottery_no][ticket_no] != true, "You did not reveal your random number. No rewards can be claimed!");
-        require(notclaimed[lottery_no][ticket_no], "You have already claimed your reward");                                      //Check the eligibility to receive prize for this ticket. See documentation for details.
+        require(ticketowner[lottery_no][ticket_no] == msg.sender, "Only the ticket owner can claim reward, and only once");      //Authenticate that entity willing to receive the price is who bought this ticket
         //We do not do state update or transfers before all checks are made to avoid reentrancy attack
         uint amount = checkIfTicketWon(lottery_no, ticket_no);                                                                   //The amount calculation logic is implemented *only* in that view function for code robustness and abstraction
         require(amount > 0, "You didn't win this time");                                                                         //Do not continue
-        notclaimed[lottery_no][ticket_no] = false;                                                                               //Disable ability of this ticket to do another withdraw to avoid reentrancy attacks
+        delete ticketowner[lottery_no][ticket_no];                                                                               //Disable ability of this ticket to do another withdraw and avoid reentrancy attacks
         require(TL_BANK.transfer(msg.sender, amount), "Transaction failed!");                                                    //
         moneyreturned[lottery_no] += amount;                                                                                     //Update auxiliary accounting records
         totalmoneyreturned += amount;
@@ -128,8 +126,8 @@ contract BULOT
     {
         require(lottery_no < getCurrentLotteryNo() - 1, "Tickets are rewarded after reveal stage ends");
         uint M = moneycollected[lottery_no];                                               //Number of tickets sold in that lottery will be used
-        require(i > 0 && 2**i < M*2, "Invalid reward number");                           //Make sure there exists a reward i
-        amount = (M / 2**i) + ((M / 2**(i-1)) % 2);                                     //The actual formula for reward of the i'th prize
+        require(i > 0 && 2**i < M*2, "Invalid reward number");                             //Make sure there exists a reward i
+        amount = (M / 2**i) + ((M / 2**(i-1)) % 2);                                        //The actual formula for reward of the i'th prize
         ticket_no = uint(keccak256(abi.encode(lotteryRandom[lottery_no], i))) % ticketcount[lottery_no];      //This is the winner calculation logic. See documentation for details.
     }
 
