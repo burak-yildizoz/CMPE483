ed8c13ec4f497d67b44a93d834db981514692200
Muhammed Enes Toptaş <44070728+EnesToptas@users.noreply.github.com>
2021-02-07 21:45:52 +0300

    Add files via upload

---
+++ nodejs/public/bulottokenabi.js
@@ -0,0 +1,230 @@
+var bulottokenabi = [
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "hash_rnd_number",
+				"type": "bytes32"
+			}
+		],
+		"name": "buyTicket",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "ticket_no",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "rnd_number",
+				"type": "uint256"
+			}
+		],
+		"name": "revealRndNumber",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "TL_contract",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"stateMutability": "nonpayable",
+		"type": "fallback"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "lottery_no",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "ticket_no",
+				"type": "uint256"
+			}
+		],
+		"name": "withdrawTicketPrize",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "lottery_no",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "ticket_no",
+				"type": "uint256"
+			}
+		],
+		"name": "checkIfTicketWon",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getCurrentLotteryNo",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "lottery_no",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "rnd_number",
+				"type": "uint256"
+			}
+		],
+		"name": "getHash",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "i",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "lottery_no",
+				"type": "uint256"
+			}
+		],
+		"name": "getIthBoughtTicketNo",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "i",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "lottery_no",
+				"type": "uint256"
+			}
+		],
+		"name": "getIthWinningTicket",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "ticket_no",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "lottery_no",
+				"type": "uint256"
+			}
+		],
+		"name": "getLastBoughtTicketNo",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "lottery_no",
+				"type": "uint256"
+			}
+		],
+		"name": "getMoneyCollected",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getTicketPrice",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	}
+]
